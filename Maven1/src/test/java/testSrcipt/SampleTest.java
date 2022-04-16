7e1dc0dd9704221132840d5fc1291096ecefcfeb
Sunil Hosamani <Sunil Hosamani@LAPTOP-CUCFLM1P>
2022-04-16 13:00:14 +0530

    commit

---
+++ Maven1/src/test/java/testSrcipt/SampleTest.java
@@ -0,0 +1,29 @@
+package testSrcipt;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleTest {
+	@Test
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("https://demo.actitime.com/login.do");
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.id("keepLoggedInCheckBoxContainer")).click();
+		driver.findElement(By.id("loginButton")).click();
+		driver.close();
+		
+		}
+
+}
